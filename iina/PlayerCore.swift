a3370480555402440d235347f036f8fb6cc5b556
Collider LI <lhc199652@gmail.com>
2017-06-21 14:01:36 +0800

    add intial window view

--- iina/PlayerCore.swift
+++ iina/PlayerCore.swift
@@ -694,6 +694,7 @@ class PlayerCore: NSObject {
     triedUsingExactSeekForCurrentFile = false
     info.fileLoading = false
     info.haveDownloadedSub = false
+    mainWindow.initialWindowView.view.isHidden = true
     DispatchQueue.main.sync {
       self.getTrackInfo()
       self.getSelectedTracks()
